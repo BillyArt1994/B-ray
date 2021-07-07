30863d6415cf5f4af7058c327718311c4ff58afe
科长 <leichen_523@qq.com>
2021-07-07 21:06:57 +0800

    多线程优化。渲染有误，需要修改。

--- CPP_RayTrcing/B-ray/Render.h
+++ CPP_RayTrcing/B-ray/Render.h
@@ -9,12 +9,41 @@
 #include "Mesh.h"
 #include "GameObject.h"
 #include "OcterTree.h"
+#include <windows.h>
+
+#define THREAD_NUM 5
+
+class RenderParams {
+public:
+	int width = 0;
+	int height = 0;
+	unsigned char * startAddr = NULL;
+
+	Vector3 camerPos = Vector3(0, 0, 0);
+	Vector3 high_left_corner = Vector3(0, 0, 0);
+	Vector3 horizontal = Vector3(0, 0, 0);
+	Vector3 vertical = Vector3(0, 0, 0);
+
+	vector<GameObject>* worldObjet = NULL;
+	OcterTree* root = NULL;
+	Light light = Light();
+
+	RenderParams(int width, int height, unsigned char * startAddr,
+		Vector3 camerPos, Vector3 high_left_corner, Vector3 horizontal, Vector3 vertical,
+		vector<GameObject>* worldObjet, OcterTree* root, Light light):
+		width(width), height(height), startAddr(startAddr), camerPos(camerPos), 
+		high_left_corner(high_left_corner), horizontal(horizontal), vertical(vertical), 
+		worldObjet(worldObjet), root(root), light(light) {}
+
+	//RenderParams() {}
+};
 
 class Render
 {
 public:
 	Render(Camera& mainCamera, Image& image, vector<GameObject>& worldObjet, Light& light) {
-		unsigned char rgb[400 * 225 * 3], *p = rgb;
+		unsigned char * rgb = (unsigned char *)malloc(400 * 225 * 3 * sizeof(char));
+		unsigned char *p = rgb;
 		int width = image.GetWidth();
 		int height = image.GetHeight();
 		Vector3 high_left_corner = mainCamera.GetLC();
@@ -26,36 +55,55 @@ public:
 		int maxLength = INTMaxAABB(worldObjet, mainCamera);
 		OcterTree root = OcterTree(worldObjet, maxLength * 2, 12, 3);
 #pragma endregion
-		for (int i = 0; i < height; i++) {
-			for (int j = 0; j < width; j++) {
-				auto u = float(j) / (width - 1);
-				auto v = float(i) / (height - 1);
-				Ray r(camerPos, ((high_left_corner + horizontal * u - vertical * v) - camerPos).normalize());
-				Color pixel_color = ray_color(r, worldObjet, root, light);
-				write_color(std::cout, pixel_color);
-				*p++ = (unsigned char)pixel_color.x();    //R
-				*p++ = (unsigned char)pixel_color.y();    //G
-				*p++ = (unsigned char)pixel_color.z();    //B
-			}
 
-			if (i % 5 == 0)
-			{
-				int rate = ceil(i*(100.0f / (height - 1)));
-				std::cout << rate << "%" << std::endl;
-			}
+		HANDLE hThread[THREAD_NUM];
+		RenderParams* params[THREAD_NUM];
+		for (int i = 0; i < THREAD_NUM; ++i) {
+			params[i] = new RenderParams(width, height / THREAD_NUM, p + i * width * (height / THREAD_NUM) * 3 ,
+								     camerPos, high_left_corner + i * vertical / THREAD_NUM, horizontal, vertical / THREAD_NUM, &worldObjet, &root, light);
+
+			hThread[i] = CreateThread(NULL, 0, PartialRender, (LPDWORD)params[i], 0, NULL);
 		}
+		WaitForMultipleObjects(THREAD_NUM, hThread, TRUE, INFINITE);
+
+		for (int i = 0; i < THREAD_NUM; ++i) {
+			delete params[i];
+		}
+
 		RenderTex(width, height, rgb);
 	};
 
 private:
 
-	void RenderTex(const unsigned image_width, const unsigned image_height, const unsigned char *rgb) {
+	//Ïß³Ìº¯Êý
+	static DWORD WINAPI PartialRender(LPVOID lpParameter)
+	{
+		struct RenderParams params = *(struct RenderParams *)lpParameter;
+		unsigned char *p = params.startAddr;
+
+		for (int i = 0; i < params.height; i++) {
+			for (int j = 0; j < params.width; j++) {
+				auto u = float(j) / params.width;
+				auto v = float(i) / params.height;
+
+				Ray r(params.camerPos, ((params.high_left_corner + params.horizontal * u - params.vertical * v) - params.camerPos).normalize());
+				Color pixel_color = ray_color(r, *(params.worldObjet), *(params.root), params.light);
+				write_color(std::cout, pixel_color);
+				*p++ = (unsigned char)pixel_color.x();    //R
+				*p++ = (unsigned char)pixel_color.y();    //G
+				*p++ = (unsigned char)pixel_color.z();    //B
+			}
+		}
+		return 0;
+	}
+
+	static void RenderTex(const unsigned image_width, const unsigned image_height, const unsigned char *rgb) {
 		FILE *fp = fopen("render.png", "wb");
 		svpng(fp, image_width, image_height, rgb, 0);
 		fclose(fp);
 	}
 
-	Color ray_color(Ray& r, vector<GameObject>& worldObjet, OcterTree& root, Light& light) {
+	static Color ray_color(Ray& r, vector<GameObject>& worldObjet, OcterTree& root, Light& light) {
 		float t = 0;
 		int index = 0;
 		bool ishit = root.Intersect(r, t, index);
@@ -119,7 +167,7 @@ private:
 
 	}
 
-	int INTMaxAABB(vector<GameObject>& worldObjet, Camera camera) {
+	static int INTMaxAABB(vector<GameObject>& worldObjet, Camera camera) {
 		float maxDis = 0;
 		for (size_t i = 0; i < worldObjet.size(); i++)
 		{
